f6cf28fc29373357a45cadf03e95a676c2dfbed1
H. Dragos <hasmasandragos13@gmail.com>
2018-02-03 11:47:42 +0200

    Changed acces token name in the source file

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def send_message(recipient_id, message_text):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+        "access_token": os.environ["ACCESS_TOKEN"]
     }
     headers = {
         "Content-Type": "application/json"
