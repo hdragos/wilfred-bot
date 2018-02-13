fd095a72c0f20fc521fd9e28f74a02b65e3c878a
H. Dragos <hasmasandragos13@gmail.com>
2018-02-13 21:27:46 +0200

    Converted from OOP to functional

--- messages.py
+++ messages.py
@@ -1,19 +1,19 @@
 import json
 import requests
-from converter import PriceConverter
+from converter import convert_one_unit
 
 def process_message(self, messageString):
     messageWords = list(messageString.strip().split())
     
     if len(messageWords) == 2:
         if messageWords[0].strip().upper() == "GET":
-            return self.__priceConverter.convert_one_unit(messageWords[1])
+            return convert_one_unit(messageWords[1])
         else:
             return "Sorry, I did not get that."
         
     if len(messageWords) == 4:
         if messageWords[0].strip().upper() == "GET" and messageWords[2].strip().upper() == "IN":
-            return self.__priceConverter.convert_one_unit(messageWords[1], messageWords[3])
+            return convert_one_unit(messageWords[1], messageWords[3])
         else:
             return "Sorry, I did not get that."
     
