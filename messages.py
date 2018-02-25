11ef3f4e55e61cbf4ed74418cbccfc6c37a0123b
H. Dragos <hasmasandragos13@gmail.com>
2018-02-25 15:14:41 +0200

    Added Regex for testing

--- messages.py
+++ messages.py
@@ -1,16 +1,19 @@
 import json
 import requests
+import re 
 from converter import convert_one_unit
 
 def process_message(messageString):
     """
-    Returns a text message
+    Returns a text message based on the messageString
     """
     messageWords = list(messageString.strip().split())
     
     #-----------Help commands----------#
+    match = re.search("HELP", messageWords[0].strip().upper())
     helpString = "Available commands:\n >Help [x]\n >Convert [x] to [y]"
-    if messageWords[0].strip().upper() == "HELP":
+    
+    if match:
         return helpString
     #----------------------------------#
     
@@ -23,6 +26,12 @@ def process_message(messageString):
         if len(messageWords) == 4:
             if messageWords[2].strip().upper() == "TO":
                 return convert_one_unit(messageWords[1], messageWords[3])
+    #----------------------------------#
+    
+    #---------Weather commands---------#
+    """
+    TO DO
+    """
     #----------------------------------#
     
     return "Sorry, I did not get that."
