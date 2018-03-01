c7110ec10feb48e1de6982c3d832812a9de6aa3a
H. Dragos <hasmasandragos13@gmail.com>
2018-03-01 14:46:17 +0200

    Regex test for help command

--- messages.py
+++ messages.py
@@ -10,7 +10,7 @@ def process_message(messageString):
     messageWords = list(messageString.strip().split())
     
     #-----------Help commands----------#
-    match = re.search("HELP", messageWords[0].strip().upper())
+    match = re.search(r"[h]{2}[e]{2}[l]{2}[p]{2}", messageWords[0].strip().lower())
     helpString = "Available commands:\n >Help [x]\n >Convert [x] to [y]"
     
     if match:
@@ -19,19 +19,18 @@ def process_message(messageString):
     
     
     #---------Convert commands---------#
-    if messageWords[0].strip().upper() == "CONVERT":
+    if messageWords[0].strip().lower() == "convert":
         if len(messageWords) == 2:
             return convert_one_unit(messageWords[1])
             
         if len(messageWords) == 4:
-            if messageWords[2].strip().upper() == "TO":
+            if messageWords[2].strip().lower() == "to":
                 return convert_one_unit(messageWords[1], messageWords[3])
     #----------------------------------#
     
     #---------Weather commands---------#
-    """
-    TO DO
-    """
+    if messageWords[0].strip().lower() == "weather":
+        return "It's raining somewhere."
     #----------------------------------#
     
     return "Sorry, I did not get that."
