bc2012590d2f3007a9060f9bbd5e325dd0d0f4c9
H. Dragos <hasmasandragos13@gmail.com>
2018-03-05 18:44:38 +0200

    Dependecies ...

---
+++ tests.py
@@ -0,0 +1,28 @@
+from message_processor import simplify_message
+import unittest
+
+class TestCaseSimplifyMessage(unittest.TestCase):
+    def setUp(self):
+        #Preparation phase
+        pass
+        
+    def tearDown(self):
+        #Cleanup phase
+        pass
+    
+    def testConvert(self):
+        inputString = "Hey, convert btc to eur for me, please"
+        actualWordList = simplify_message(inputString)
+        expectedWordList = ["convert", "btc", "to", "eur"]
+        
+        self.assertEqual(actualWordList, expectedWordList, "Test 1: Failed. Invalid conversion simplification")
+        
+    def testHelp(self):
+        inputString = "Hello, i would need some help"
+        actualWordList = simplify_message(inputString)
+        expectedWordList = ["help"]
+        
+        self.assertEqual(actualWordList, expectedWordList, "Test 2: Failed. Invalid help simplification")
+
+if __name__ == '__main__':
+    unittest.main()
